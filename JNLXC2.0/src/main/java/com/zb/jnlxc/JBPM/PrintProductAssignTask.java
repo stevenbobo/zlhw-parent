3bca8553bb2028c381189874adad4a5f7339a604
郑波 <279360619@qq.com>
2013-07-21 11:09:24 +0800

    a

--- JNLXC2.0/src/main/java/com/zb/jnlxc/JBPM/PrintProductAssignTask.java
+++ JNLXC2.0/src/main/java/com/zb/jnlxc/JBPM/PrintProductAssignTask.java
@@ -1,5 +1,6 @@
 package com.zb.jnlxc.JBPM;
 
+import com.zb.jnlxc.model.Admin;
 import com.zb.jnlxc.service.AdminService;
 import com.zb.jnlxc.service.ProductTeamService;
 
@@ -11,12 +12,18 @@ import com.ZLHW.base.factory.BeanFactory;
 import com.zb.jnlxc.model.ProductTeam;
 import com.zb.jnlxc.service.BusinessService;
 
+import java.util.List;
+
 public class PrintProductAssignTask implements AssignmentHandler{
 	public void assign(Assignable assignable, OpenExecution execution) {
         AdminService adminService=(AdminService)BeanFactory.LookUp("adminService");
         ProductTeamService productTeamService=(ProductTeamService)BeanFactory.LookUp("productTeamService");
         Integer productTeamId = (Integer)execution.getVariable("productTeamId");
 		ProductTeam team=productTeamService.getById(productTeamId);
+        List<Admin> list=adminService.getPowerAdmins("机台负责人打印生产跟踪单");
+        for(Admin admin:list){
+            assignable.addCandidateUser(admin.getAccount());
+        }
 	    assignable.addCandidateUser(adminService.loadById(team.getCharge_dbId()).getAccount());
 	  }
 }
