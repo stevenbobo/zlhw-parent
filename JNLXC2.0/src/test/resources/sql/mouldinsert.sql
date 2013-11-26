4309e28b7781f88732474789fb8682bedc35e39e
zhengbo.zb <279360619@qq.com>
2013-11-26 10:42:54 +0800

    sql

---
+++ JNLXC2.0/src/test/resources/sql/mouldinsert.sql
@@ -0,0 +1,35 @@
+
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(801, '', '', '添加模具(发单)', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(802, '', '', '模具入场验收', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(803, '', '', '入厂责任人分配', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(804, '', '', '新模技术检修处理', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(805, '', '', '返修退模出厂确认', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(806, '', '', '报废退模申请', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(807, '', '', '返修模具申请', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(808, '', '', '抛排新模处理', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(809, '', '', '挤压模具加温确认', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(810, '', '', '挤压班组收模', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(811, '', '', '氮化开炉确认', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(812, '', '', '氮化出炉确认', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(813, '', '', '新模合格入库', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(814, '', '', '挤压生产（试模）使用', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(815, '', '', '泡模收模确认', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(816, '', '', '抛排收模处理', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(817, '', '', '(试模)异常模转修处理', '', NULL, 0, NULL, 8, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(818, '', '', '返修及修改尺寸入场确认', '', NULL, 0, NULL, 8, NULL);
+
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(901, '', '', '仓库发模', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(902, '', '', '挤压班组收模', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(903, '', '', '挤压模具加温确认', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(904, '', '', '挤压生产(试模)使用', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(905, '', '', '泡模收模确认', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(906, '', '', '抛排收模处理', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(907, '', '', '(试模)异常模转修处理', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(908, '', '', '氮化限产填写', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(909, '', '', '氮化开炉确认', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(910, '', '', '氮化出炉确认', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(911, '', '', '返修模具申请', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(912, '', '', '报废退模申请', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(913, '', '', '返修退模出厂确认', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(914, '', '', '生产模具回库', '', NULL, 0, NULL, 9, NULL);
+INSERT INTO `jnlxc_authordetail` (`dbId`, `beanFunction`, `beanName`, `name`, `path`, `selected`, `status`, `version`, `parentId`, `authorLever`) VALUES(915, '', '', '排模确认', '', NULL, 0, NULL, 9, NULL);
